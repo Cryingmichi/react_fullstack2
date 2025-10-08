d087356c4c9344e4e215a472e11fbd1f1e3b55b3
Cryingmichi <di.alarconl@duocuc.cl>
2025-10-08 10:58:24 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import FormularioLibro from './components/FormularioLibro'
 
 function App() {
   const [libros, setLibros] = useState([])
+  const [libroModificar, setLibroModificar, setLibroEditando] = useState(null)
   useEffect(()=>{
     fetch("src/data/libros.json")
     .then((res)=>res.json())
@@ -17,15 +18,28 @@ function App() {
   const eliminarLibro = (id) => {
     setLibros(libros.filter((l)=>l.id !== id))
   }
+  const editarLibro = (libroActualizado) => {
+    setLibros(libros.map((li)=>(li.id === libroActualizado.id ? libroActualizado : li)))
+  }
   return (
     <div className='container mt-3'>
       <h1>Formulario Ingreso Libros</h1>
-      <FormularioLibro agregar={agregarLibro}/>
+      <FormularioLibro agregar={agregarLibro}
+      libroEditando={libroModificar}
+      editar={editarLibro}
+      setLibroEditando={setLibroModificar}/>
+      {libros.length === 0 && <p>No hay libros disponibles</p>}
+      {libros.length>5 && <p>Tienes una gran cantidad de libros</p>}
       <div className='row'>
         {
           libros.map(libro => (
             <div className='col-md-4' key={libro.id}>
-              <Card titulo={libro.titulo} contenido={libro.contenido} eliminar={() => eliminarLibro(libro.id)}/>
+              <Card titulo={libro.titulo} 
+              contenido={libro.contenido} 
+              imagen={libro.imagen} 
+              eliminar={() => eliminarLibro(libro.id)}
+              modificar={ () => setLibroModificar(libro) }
+              />
             </div>
           ))
         }
--- src/components/FormularioLibro.jsx
+++ src/components/FormularioLibro.jsx
@@ -1,20 +1,41 @@
-import { useState } from "react";
-function FormularioLibro( Agregar) {
-    const { titulo } = useState("");
-    const { contenido } = useState("");
+import { useEffect, useState } from "react";
+function FormularioLibro( Agregar,libroEditando,editar,setLibroEditando ){ 
+    const { titulo, setTitulo } = useState("");
+    const { contenido, setContenido } = useState("");
+    useEffect(() => {
+        if (libroEditando) {
+            setTitulo(libroEditando.titulo)
+            setContenido(libroEditando.contenido)
+        }
+    }, [libroEditando])
     const handleSubmit = (event) => {
         event.preventDefault();
          if(!titulo||!contenido)return
-         agregar({id:Date.now(),titulo, contenido})
+
+         if(libroEditando){
+            editar({id:libroEditando.id,titulo, contenido})
+            setLibroEditando(null)
+         }else{
+            agregar({id:Date.now(),titulo, contenido})
+         }
          setTitulo("")
          setContenido("")
         }
 
+
          return(
         <form className="mb-4" onSubmit={handleSubmit}>
-            <input type="text" placeholder="Ingrese el título" onChange={(e)=> setTitulo(e.target.value)}/>
-            <input type="text" placeholder="Ingrese el contenido" onChange={(e)=> setContenido(e.target.value)}/>
-            <button type="submit" className="btn btn-success">Agregar Libro</button>
+            <input type="text" 
+            placeholder="Ingrese el título" 
+            value={titulo}
+            onChange={(e)=> setTitulo(e.target.value)}/>
+            <input type="text" 
+            placeholder="Ingrese el contenido"
+            value={contenido}
+            onChange={(e)=> setContenido(e.target.value)}/>
+            <button type="submit" className={libroEditando ? "btn btn-warning" : "btn btn-success"}>
+                {libroEditando ? "Actualizar libro" : "Agregar Libro"}
+            </button>
         </form>
     )
 }
